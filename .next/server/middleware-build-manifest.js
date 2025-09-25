02e33173efbc0683352e114238819a80172591e2
Kweku Baah <baahkweku60@gmail.com>
2025-09-25 16:00:12 +0000

    fix deployement error

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -1,1 +1,18 @@
-self.__BUILD_MANIFEST={polyfillFiles:["static/chunks/polyfills-42372ed130431b0a.js"],devFiles:[],ampDevFiles:[],lowPriorityFiles:[],rootMainFiles:["static/chunks/webpack-03c87f68c10117d4.js","static/chunks/fd9d1056-d6a90c4eacbc1d68.js","static/chunks/117-e9ffca2d029c192b.js","static/chunks/main-app-2a9f2209a1f57ce9.js"],pages:{"/_app":["static/chunks/webpack-03c87f68c10117d4.js","static/chunks/framework-f66176bb897dc684.js","static/chunks/main-7dd95842f7277fb0.js","static/chunks/pages/_app-72b849fbd24ac258.js"],"/_error":["static/chunks/webpack-03c87f68c10117d4.js","static/chunks/framework-f66176bb897dc684.js","static/chunks/main-7dd95842f7277fb0.js","static/chunks/pages/_error-7ba65e1336b92748.js"]},ampFirstPages:[]},self.__BUILD_MANIFEST.lowPriorityFiles=["/static/"+process.env.__NEXT_BUILD_ID+"/_buildManifest.js",,"/static/"+process.env.__NEXT_BUILD_ID+"/_ssgManifest.js"];
+self.__BUILD_MANIFEST = {
+  "polyfillFiles": [
+    "static/chunks/polyfills.js"
+  ],
+  "devFiles": [],
+  "ampDevFiles": [],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [],
+  "pages": {
+    "/_app": []
+  },
+  "ampFirstPages": []
+};
+self.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
+
+];
